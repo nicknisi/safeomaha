4061256bd2c905ff55f2ed50813ed2e9e4f848c7
Nick Nisi <nick@nisi.org>
2012-04-14 02:51:42 -0500

    Added basic html page
    + Added Twitter Bootstrap to page
    + Added jQuery to page
    + Added dojo to page
    + created dojo configuration script (run.js)

---
+++ web/js/run.js
@@ -0,0 +1,9 @@
+require({
+    baseUrl : 'js',
+    packages : [
+        { name : 'dojo', location : 'dojo/dojo' },
+        { name : 'dijit', location : 'dojo/dijit' },
+        { name : 'dojox', location : 'dojo/dojox' }
+    ],
+    cache : {}
+});
