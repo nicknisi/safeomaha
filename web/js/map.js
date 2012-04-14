ea3fb9ec5fb2390b1331eb8d03ee3e1d43af38b5
Ryan Stille <ryan@stillnet.org>
2012-04-14 15:48:16 -0500

    Mocked up data types. Added different color makers for different data types.

--- web/js/map.js
+++ web/js/map.js
@@ -4,33 +4,52 @@ function initialize() {
 	zoom: 12,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+	
+	// load data
+	var dataSet = fetchData();
+	
+	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		var markers = new Array();
+		for (var i = 0; i < dataSet.length; i++) {
+			markers[i] = new google.maps.Marker({
+				position: new google.maps.LatLng(dataSet[i].lat, dataSet[i].lng),
+				title: dataSet[i].title,
+				map: map
+			});
+			
+			if (markerImages[dataSet[i].type])
+				markers[i].setIcon(markerImages[dataSet[i].type]);
+			else
+			
+			console.log('test ' + markers[i].getTitle());
+			
+		}
 
-var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	var markers = new Array();
-	for (var i = 0; i < dataSet.length; i++) {
-		markers[i] = new google.maps.Marker({
-			position: new google.maps.LatLng(dataSet[i].Lat, dataSet[i].Lng),
-			title: dataSet[i].title,
-			map: map
-		});
-		console.log(markers[i].getTitle());
-		
 	}
 
+function fetchData() {
+	// place holder function. This will fetch data later
+	var dataSet =
+		[
+		  {
+			  "title": "Some point",
+			  "lat": 41.25768,
+			  "lng": -95.9442,
+			  "type": "crime"
+		  },
+		  {
+			  "title": "Another point",
+			  "lat": 41.26916,
+			  "lng": -95.9418,
+			  "type": "trafficStop"
+		  }
+		];
+
+	return dataSet;
 }
 
-// fake an array of points. This would be returned by an ajax call
-var dataSet =
-	[
-	  {
-		  "title": "Some point",
-		  "Lat": 41.25768,
-		  "Lng": -95.9442
-	  },
-	  {
-		  "title": "Another point",
-		  "Lat": 41.26916,
-		  "Lng": -95.9418
-	  }
-	];
-		
+// define images for different types of data (http://jg.org/mapping/icons.html)
+var markerImages = {
+	"crime": "http://labs.google.com/ridefinder/images/mm_20_red.png",
+	"trafficStop": "http://labs.google.com/ridefinder/images/mm_20_white.png"
+};
