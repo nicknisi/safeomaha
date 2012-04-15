c80fbbdde512dc057d18b4dd09c29faf4db46b0e
Nick Nisi <nick@nisi.org>
2012-04-15 14:24:02 -0500

    Added accordion to index

--- web/js/run.js
+++ web/js/run.js
@@ -6,4 +6,26 @@ require({
         { name : 'dojox', location : 'dojo/dojox' }
     ],
     cache : {}
+}, [
+    'dijit/form/Button',
+    'dijit/layout/AccordionContainer',
+    'dijit/layout/ContentPane',
+    'dojo/domReady!'
+], function (Button, AccordionContainer, ContentPane) {
+    var accordion = new AccordionContainer({
+        height: '400px'
+    }, "facts-widget");
+    accordion.addChild(new ContentPane({
+        title : 'Crime Facts',
+        content : 'foo'
+    }));
+    accordion.addChild(new ContentPane({
+        title : 'Police Facts',
+        content : 'bar'
+    }));
+    accordion.addChild(new ContentPane({
+        title : 'Accidents Facts',
+        content : 'baz'
+    }));
+    accordion.startup();
 });
