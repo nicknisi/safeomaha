d0d90a1b2244b2e984734457888fec99d2a38d59
Nick Nisi <nick@nisi.org>
2012-04-15 15:12:23 -0500

    Added FourSquare OAuth url

--- web/js/run.js
+++ web/js/run.js
@@ -7,25 +7,10 @@ require({
     ],
     cache : {}
 }, [
-    'dijit/form/Button',
+    'dojo/on',
+    'dojo/dom',
     'dijit/layout/AccordionContainer',
     'dijit/layout/ContentPane',
     'dojo/domReady!'
-], function (Button, AccordionContainer, ContentPane) {
-    //var accordion = new AccordionContainer({
-        //height: '400px'
-    //}, "facts-widget");
-    //accordion.addChild(new ContentPane({
-        //title : 'Crime Facts',
-        //content : 'foo'
-    //}));
-    //accordion.addChild(new ContentPane({
-        //title : 'Police Facts',
-        //content : 'bar'
-    //}));
-    //accordion.addChild(new ContentPane({
-        //title : 'Accidents Facts',
-        //content : 'baz'
-    //}));
-    //accordion.startup();
+], function (on, dom, Button, AccordionContainer, ContentPane) {
 });
